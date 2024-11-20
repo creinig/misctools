8a1b453d4843b9fcc95a4c7fbebccb99f6b78b47
Christian Reiniger <reiniger@weclapp.com>
2024-11-20 10:28:21 +0100

    add durationcalc.rb

--- Gemfile
+++ Gemfile
@@ -6,3 +6,5 @@ gem "standard", "~> 1.41"
 gem "simplecov", "~> 0.22.0"
 
 gem "rake", "~> 13.2"
+
+gem "ruby-units", "~> 4.1"
---
+++ bin/durationcalc.rb
@@ -0,0 +1,47 @@
+#!/usr/bin/env ruby -w
+
+require "bundler/setup"
+require "ruby-units"
+
+def toHM(unit)
+  mins = unit.convert_to("min").scalar.to_i
+  sprintf("%d:%02d", mins/60, mins%60)
+end
+
+def pcurrent
+  puts "Current value: #{toHM(@duration)}"
+end
+
+
+@duration = "0:00".to_unit
+
+# Output help text
+puts "Duration calculator."
+puts "Enter one duration (HH:MM) per line to "
+puts "  * subtract from the current value (prefix '-')"
+puts "  * overwrite the current value (prefix '=')"
+puts "  * add to the current value (anything else)"
+puts "Exit with Ctrl-D or 'x'"
+puts
+pcurrent
+
+while line = gets 
+  case line.strip
+  when /^=\s*(\d+:\d+)/ 
+    @duration = $~[1].to_unit
+  when /^-\s*(\d+:\d+)/ 
+    @duration -= $~[1].to_unit
+  when /\d+:\d+/
+    @duration += $~[0].to_unit
+  when /^$/
+    # NOP
+  when /^x$/
+    exit
+  else
+    puts "Invalid input data"
+  end
+
+  pcurrent
+end
+
+
