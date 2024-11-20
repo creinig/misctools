d649ec7be0bd39283cad5f3400a15025750a36f6
Christian Reiniger <creinig@creinig.de>
2024-11-20 08:47:14 +0100

    add tiddlytags.rb

---
+++ bin/tiddlytags.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby -w
+
+require 'set'
+
+PATH= ARGV[0] || "#{ENV['HOME']}/wiki/bob/tiddlers"
+tags = Set.new
+
+Dir.glob("#{PATH}/*.tid").each do |file|
+
+  File.open file do |f|
+    f.take_while{|line| not line.strip.empty?}.each do |line|
+      if line =~ /^tags:\s*(\w.*)$/
+        tags.merge $1.split
+        break
+      end
+    end
+  end
+end
+
+puts tags.to_a.sort.join("\n")
+
