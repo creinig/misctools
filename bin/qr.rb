a408b7635caa5c4db577f3d0d517f9ac1b24cb1d
Christian Reiniger <creinig@creinig.de>
2024-11-24 22:33:03 +0100

    add png,svg support to qr.rb

--- bin/qr.rb
+++ bin/qr.rb
@@ -2,18 +2,37 @@
 
 require "bundler/setup"
 require "rqrcode"
+require "getopt_ext"
 
-if ARGV.empty?
-  puts "Usage: qr.rb <text to encode>"
+opts = GetoptLong.new(
+  opt_flag("help"),
+  opt_flag("png"),
+  opt_flag("svg"),
+  opt_flag("ansi")
+)
+
+if opts.empty? || opts.include?("help") || ARGV.empty?
+  puts <<-HELP
+  Usage: qr.rb [options] <text to encode>
+  Options:
+    --help, -h : show this help
+    --ansi, -a : render as ansi (default)
+    --png,  -p : render as png (redirect to file!)
+    --svg,  -s : render as svg (redirect to file!)
+  HELP
   exit 1
 end
 
 qrcode = RQRCode::QRCode.new(ARGV[0])
 
-qr = qrcode.as_ansi(
-  light: "\033[47m", dark: "\033[40m",
-  fill_character: "  ",
-  quiet_zone_size: 2
-)
-
-puts qr
+if opts.include?("png")
+  puts qrcode.as_png
+elsif opts.include?("svg")
+  puts qrcode.as_svg
+else
+  puts qrcode.as_ansi(
+    light: "\033[47m", dark: "\033[40m",
+    fill_character: "  ",
+    quiet_zone_size: 2
+  )
+end
